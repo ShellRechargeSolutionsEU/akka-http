9cf40fedf124a2bc8785005cef681394e12f658a
Yaroslav Klymko <t3hnar@gmail.com>
2012-04-25 14:39:03 +0300

    Using ActorSystem instead of HttpSystem

--- src/test/scala/com/thenewmotion/akka/http/ConfigSpec.scala
+++ src/test/scala/com/thenewmotion/akka/http/ConfigSpec.scala
@@ -1,9 +1,5 @@
 package com.thenewmotion.akka.http
 
-/**
- * Copyright (C) 2009-2011 Typesafe Inc. <http://www.typesafe.com>
- */
-
 import org.specs2.mutable.SpecificationWithJUnit
 import akka.actor.ActorSystem
 
@@ -11,7 +7,7 @@ class ConfigSpec extends SpecificationWithJUnit {
 
   "The default configuration file 'reference.conf'" should {
     "contain properties for akka-http that are used in code with their correct defaults" >> {
-      val config = HttpSystem().settings.config
+      val config = ActorSystem().settings.config
       import config._
       getBoolean("akka.http.connection-close") mustEqual true
       getString("akka.http.expired-header-name") mustEqual "Async-Timeout"
