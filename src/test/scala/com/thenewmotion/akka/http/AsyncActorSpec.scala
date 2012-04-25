847375de0ee0e8b7415b30f6f7b7d67e70d64390
Yaroslav Klymko <t3hnar@gmail.com>
2012-04-25 14:38:29 +0300

    Cleanup in AsyncActorSpec

--- src/test/scala/com/thenewmotion/akka/http/AsyncActorSpec.scala
+++ src/test/scala/com/thenewmotion/akka/http/AsyncActorSpec.scala
@@ -2,45 +2,31 @@ package com.thenewmotion.akka.http
 
 import org.specs2.mock.Mockito
 import org.specs2.specification.Scope
-import org.specs2.mutable.SpecificationWithJUnit
-import akka.testkit.{TestProbe, TestActorRef}
-import akka.util.DurationInt
 import javax.servlet.AsyncEvent
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import Listener._
 import AsyncActor._
 import EndpointsActor._
+import org.specs2.mutable.SpecificationWithJUnit
+import akka.testkit.{TestKit, TestActorRef}
+import akka.actor.{ActorSystem, Props}
 
 
 /**
  * @author Yaroslav Klymko
  */
+//TODO maybe it's FSM
 class AsyncActorSpec extends SpecificationWithJUnit with Mockito {
 
-  abstract class HttpContext extends Scope {
+  abstract class HttpContext extends TestKit(ActorSystem()) with Scope {
 
-    implicit val system = HttpSystem()
-    val probe = TestProbe()
+    system.actorOf(Props(_ => {
+      case msg => testActor ! msg
+    }), "endpoints")
 
     val asyncContext = AsyncContextMock()
-    val actorRef = TestActorRef(new AsyncActor(asyncContext) {
-      override def endpointsActor = probe.ref
-    })
 
-    def test()
-
-    def apply() {
-      try test()
-      finally system.shutdown()
-    }
-
-    apply()
-
-    def expectFindEndpoint() {
-      probe.within(new DurationInt(200).millis) {
-        probe.expectMsgType[Find]
-      }
-    }
+    val actorRef = TestActorRef(new AsyncActor(asyncContext))
 
     def asyncEventMessage(on: OnEvent) = AsyncEventMessage(new AsyncEvent(asyncContext, new Exception), on)
 
@@ -49,127 +35,120 @@ class AsyncActorSpec extends SpecificationWithJUnit with Mockito {
 
   "AsyncActor" should {
     "look for defined endpoint for current request when started" >> new HttpContext {
-      def test() {
-        actorRef ! Start
-        expectFindEndpoint()
-        probe.sender ! Found(_ => _ => _ => Unit)
-        there was one(asyncContext).complete()
-        actorRef.isTerminated must beTrue
-      }
+      actorRef ! Start
+      expectMsgType[Find]
+      actorRef ! Found(_ => _ => _ => Unit)
+      there was one(asyncContext).complete()
+      actorRef.isTerminated must beTrue
     }
     "stop self on Error event" >> new HttpContext {
-      def test() {
-        actorRef ! Start
-        actorRef ! asyncEventMessage(OnError)
-        actorRef.isTerminated must beTrue
-      }
+      actorRef ! Start
+      actorRef ! asyncEventMessage(OnError)
+      actorRef.isTerminated must beTrue
     }
     "stop self on Complete event" >> new HttpContext {
-      def test() {
-        actorRef ! Start
-        actorRef ! asyncEventMessage(OnComplete)
-        actorRef.isTerminated must beTrue
-      }
+      actorRef ! Start
+      actorRef ! asyncEventMessage(OnComplete)
+      actorRef.isTerminated must beTrue
     }
     "stop self on Timeout event" >> new HttpContext {
-      def test() {
-        actorRef ! Start
-        actorRef ! asyncEventMessage(OnTimeout)
-        actorRef.isTerminated must beTrue
+      actorRef ! Start
+
+      var called = false
+
+      def completion(res: HttpServletResponse): (Boolean => Unit) = {
+        called = true
+        (b: Boolean) => ()
       }
+
+      expectMsgType[Find]
+      actorRef ! asyncEventMessage(OnTimeout)
+      actorRef ! Found(_ => completion)
+      actorRef.isTerminated must beTrue
+      called must beFalse
     }
     "response if async not completed" >> new HttpContext {
-      def test() {
-        actorRef ! Start
+      actorRef ! Start
 
-        var called = false
-        def completion(res: HttpServletResponse): (Boolean => Unit) = {
-          called = true
-          (b: Boolean) => ()
-        }
+      var called = false
 
-        expectFindEndpoint()
-        probe.sender ! Found(_ => completion)
-        there was one(asyncContext).complete()
-        called must beTrue
+      def completion(res: HttpServletResponse): (Boolean => Unit) = {
+        called = true
+        (b: Boolean) => ()
       }
+
+      expectMsgType[Find]
+      actorRef ! Found(_ => completion)
+      there was one(asyncContext).complete()
+      called must beTrue
     }
     "not response if async already completed" >> new HttpContext {
-      def test() {
-        actorRef ! Start
+      actorRef ! Start
 
-        var called = false
-        def completion(res: HttpServletResponse): (Boolean => Unit) = {
-          called = true
-          (b: Boolean) => ()
-        }
+      var called = false
 
-        def func(req: HttpServletRequest): (HttpServletResponse => Boolean => Unit) = {
-          actorRef ! asyncEventMessage(OnTimeout)
-          completion
-        }
-
-        expectFindEndpoint()
-        probe.sender ! Found(func)
-
-        there was no(asyncContext).complete()
-        called must beFalse
+      def completion(res: HttpServletResponse): (Boolean => Unit) = {
+        called = true
+        (b: Boolean) => ()
       }
+
+      def func(req: HttpServletRequest): (HttpServletResponse => Boolean => Unit) = {
+        actorRef ! asyncEventMessage(OnTimeout)
+        completion
+      }
+
+      expectMsgType[Find]
+      actorRef ! Found(func)
+
+      there was no(asyncContext).complete()
+      called must beFalse
     }
     "response with 'Status Code 500' when exception while processing request" >> new HttpContext {
-      def test() {
-        actorRef ! Start
+      actorRef ! Start
 
-        def func(req: HttpServletRequest): (HttpServletResponse => Boolean => Unit) = {
-          throw new Exception
-        }
-
-        expectFindEndpoint()
-        probe.sender ! Found(func)
-
-        there was one(res).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
-        there was one(asyncContext).complete()
+      def func(req: HttpServletRequest): (HttpServletResponse => Boolean => Unit) = {
+        throw new Exception
       }
+
+      expectMsgType[Find]
+      actorRef ! Found(func)
+
+      there was one(res).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
+      there was one(asyncContext).complete()
     }
     "response with 'Status Code 500' when exception while responding" >> new HttpContext {
-      def test() {
-        actorRef ! Start
+      actorRef ! Start
 
-        def func(res: HttpServletResponse): (Boolean => Unit) = {
-          throw new Exception
-        }
-
-        expectFindEndpoint()
-        probe.sender ! Found(_ => func)
-
-        there was one(res).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
-        there was one(asyncContext).complete()
+      def func(res: HttpServletResponse): (Boolean => Unit) = {
+        throw new Exception
       }
+
+      expectMsgType[Find]
+      actorRef ! Found(_ => func)
+
+      there was one(res).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
+      there was one(asyncContext).complete()
     }
     "pass to enpoint true if completed successfully" >> new HttpContext {
-      def test() {
-        actorRef ! Start
+      actorRef ! Start
 
-        expectFindEndpoint()
-        var result = false
-        probe.sender ! Found(_ => _ => result = _)
+      expectMsgType[Find]
+      var result = false
+      actorRef ! Found(_ => _ => result = _)
 
-        there was one(asyncContext).complete()
-        result must beTrue
-      }
+      there was one(asyncContext).complete()
+      result must beTrue
     }
     "pass to endpoint false if not completed successfully" >> new HttpContext {
-      def test() {
-        actorRef ! Start
+      actorRef ! Start
 
-        expectFindEndpoint()
-        asyncContext.complete() throws (new RuntimeException)
-        var result = true
-        probe.sender ! Found(_ => _ => result = _)
+      expectMsgType[Find]
+      asyncContext.complete() throws (new RuntimeException)
+      var result = true
+      actorRef ! Found(_ => _ => result = _)
 
-        there was one(asyncContext).complete()
-        result must beFalse
-      }
+      there was one(asyncContext).complete()
+      result must beFalse
     }
   }
 }
